e5d674ee191f2cee508a8c4a2768b2a582a6c0ce
Simon <alazarsimon@outlook.com>
2026-08-07 16:23:31 -0400

    Corrected ci.yml again (I hope I don't have to keep correcting this)

--- MLTests/serialization/serializationTests.cpp
+++ MLTests/serialization/serializationTests.cpp
@@ -21,7 +21,7 @@ TEST_SUITE("Serialization Tests") {
 	TEST_CASE("Binary Archive Tests") {
 		SUBCASE("Write and Read basic data to a file") {
 			{
-				std::ofstream out{ "../../models/testFiles/test1.ckpt", std::ios::binary };
+				std::ofstream out{ "models/testFiles/test1.ckpt", std::ios::binary };
 
 				BinaryWriter writer(out);
 
@@ -29,7 +29,7 @@ TEST_SUITE("Serialization Tests") {
 				writer.Write(test);
 			}
 
-			std::ifstream in{ "../../models/testFiles/test1.ckpt", std::ios::binary };
+			std::ifstream in{ "models/testFiles/test1.ckpt", std::ios::binary };
 
 			BinaryReader reader{ in };
 
@@ -41,7 +41,7 @@ TEST_SUITE("Serialization Tests") {
 
 		SUBCASE("Write and Read an array/string to a file") {
 			{
-				std::ofstream out{ "../../models/testFiles/test2.ckpt", std::ios::binary };
+				std::ofstream out{ "models/testFiles/test2.ckpt", std::ios::binary };
 
 				BinaryWriter writer{ out };
 
@@ -52,7 +52,7 @@ TEST_SUITE("Serialization Tests") {
 				writer.WriteArray(string.data(), size);
 			}
 
-			std::ifstream in{ "../../models/testFiles/test2.ckpt", std::ios::binary };
+			std::ifstream in{ "models/testFiles/test2.ckpt", std::ios::binary };
 
 			BinaryReader reader{ in };
 
@@ -69,7 +69,7 @@ TEST_SUITE("Serialization Tests") {
 
 		SUBCASE("Write and Read a tensor to a file") {
 			{
-				std::ofstream out{ "../../models/testFiles/test3.ckpt", std::ios::binary };
+				std::ofstream out{ "models/testFiles/test3.ckpt", std::ios::binary };
 
 				BinaryWriter writer{ out };
 
@@ -78,7 +78,7 @@ TEST_SUITE("Serialization Tests") {
 				writer.WriteTensor(tensor);
 			}
 
-			std::ifstream in{ "../../models/testFiles/test3.ckpt", std::ios::binary };
+			std::ifstream in{ "models/testFiles/test3.ckpt", std::ios::binary };
 
 			BinaryReader reader{ in };
 
@@ -111,14 +111,14 @@ TEST_SUITE("Serialization Tests") {
 				}
 			}
 
-			Checkpoint::Save(model, "../../models/testFiles/checkpointTest2.ckpt");
+			Checkpoint::Save(model, "models/testFiles/checkpointTest1.ckpt");
 
 			Sequential<float> model2;
 			model2.Emplace<LinearLayer<float>>(2, 3);
 			model2.Emplace<LeakyReLULayer<float>>(0.1f);
 			model2.Emplace<LinearLayer<float>>(3, 1);
 
-			Checkpoint::Load(model2, "../../models/testFiles/checkpointTest2.ckpt");
+			Checkpoint::Load(model2, "models/testFiles/checkpointTest1.ckpt");
 
 			auto parameters2 = model2.GetParameters();
 
@@ -150,14 +150,14 @@ TEST_SUITE("Serialization Tests") {
 				}
 			}
 
-			Checkpoint::Save(model, "../../models/testFiles/checkpointTest2.ckpt");
+			Checkpoint::Save(model, "models/testFiles/checkpointTest2.ckpt");
 
 			Sequential<float> model2;
 			model2.EmplaceNamed<LinearLayer<float>>("fc1", 2, 3);
 			model2.EmplaceNamed<LeakyReLULayer<float>>("leakyRelu", 0.1f);
 			model2.EmplaceNamed<LinearLayer<float>>("fc2", 3, 1);
 
-			Checkpoint::Load(model2, "../../models/testFiles/checkpointTest2.ckpt");
+			Checkpoint::Load(model2, "models/testFiles/checkpointTest2.ckpt");
 
 			auto parameters2 = model2.GetParameters();
 
@@ -191,7 +191,7 @@ TEST_SUITE("Serialization Tests") {
 
 			SGD opt{ parameters, 0.1f, 0.5f };
 
-			Checkpoint::Save(model, "../../models/testFiles/checkpointTest3.ckpt", &opt);
+			Checkpoint::Save(model, "models/testFiles/checkpointTest3.ckpt", &opt);
 
 			Sequential<float> model2;
 			model2.Emplace<LinearLayer<float>>(2, 3);
@@ -202,7 +202,7 @@ TEST_SUITE("Serialization Tests") {
 
 			SGD opt2{ parameters, 0.2f, 0.2f }; /// Learning rate and weight decay should be different after loading
 
-			Checkpoint::Load(model2, "../../models/testFiles/checkpointTest3.ckpt", &opt2);
+			Checkpoint::Load(model2, "models/testFiles/checkpointTest3.ckpt", &opt2);
 
 			for (auto& ref : parameters2) {
 				auto& param = ref.get();
@@ -245,7 +245,7 @@ TEST_SUITE("Serialization Tests") {
 
 			StepLR scheduler{ opt, 2, 0.9f };
 
-			Checkpoint::Save(model, "../../models/testFiles/checkpointTest4.ckpt", &opt, &scheduler);
+			Checkpoint::Save(model, "models/testFiles/checkpointTest4.ckpt", &opt, &scheduler);
 
 			Sequential<float> model2;
 			model2.Emplace<LinearLayer<float>>(2, 3);
@@ -258,7 +258,7 @@ TEST_SUITE("Serialization Tests") {
 
 			StepLR scheduler2{ opt2, 3, 0.2f }; /// Step size and gamma should be different after loading
 
-			Checkpoint::Load(model2, "../../models/testFiles/checkpointTest4.ckpt", &opt2, &scheduler2);
+			Checkpoint::Load(model2, "models/testFiles/checkpointTest4.ckpt", &opt2, &scheduler2);
 
 			for (auto& ref : parameters2) {
 				auto& param = ref.get();
@@ -315,13 +315,13 @@ TEST_SUITE("Serialization Tests") {
 
 			auto state = trainer.GetState();
 
-			Checkpoint::Save<float>(model, "../../models/testFiles/checkpointTest5.ckpt", nullptr, nullptr, &state);
+			Checkpoint::Save<float>(model, "models/testFiles/checkpointTest5.ckpt", nullptr, nullptr, &state);
 
 			Trainer<float> trainer2{ model, opt, lossFn };
 
 			auto state2 = trainer2.GetState();
 
-			Checkpoint::Load<float>(model, "../../models/testFiles/checkpointTest5.ckpt", nullptr, nullptr, &state2);
+			Checkpoint::Load<float>(model, "models/testFiles/checkpointTest5.ckpt", nullptr, nullptr, &state2);
 
 			trainer2.LoadState(state2);
 
